0de189cee637d7def3b1a14e2343ff610d0ed4bd
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-12-02 10:57:26 -0500

    Solve day 2 problem 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,26 +1,46 @@
 use crate::utils::read_lines;
 
 pub fn solution1() {
-    let reports = read_lines("src/day2/input.txt").map(|line| {
+    let reports = get_reports();
+    let safe_reports = reports
+        .filter(|report| report.windows(3).all(window_is_valid))
+        .count();
+
+    println!("Number of safe reports = {safe_reports}");
+}
+
+pub fn solution2() {
+    let reports = get_reports();
+    let safe_reports = reports
+        .filter(|report| {
+            report.windows(3).all(window_is_valid)
+                || (0..report.len()).any(|i| {
+                    [&report[0..i], &report[i + 1..]]
+                        .concat()
+                        .windows(3)
+                        .all(window_is_valid)
+                })
+        })
+        .count();
+
+    println!("Number of safe reports = {safe_reports}");
+}
+
+fn window_is_valid(window: &[usize]) -> bool {
+    matches!(window[0].abs_diff(window[1]), 1..=3)
+        && matches!(window[1].abs_diff(window[2]), 1..=3)
+        && ((window[0] > window[1] && window[1] > window[2])
+            || (window[0] < window[1] && window[1] < window[2]))
+}
+
+fn get_reports() -> impl Iterator<Item = Vec<usize>> {
+    read_lines("src/day2/input.txt").map(|line| {
         line.split_ascii_whitespace()
             .map(|level| {
                 level
                     .parse()
                     .expect("Reactor level is always valid integer")
             })
-            .collect::<Vec<usize>>()
-    });
-
-    let safe_reports = reports
-        .filter(|report| {
-            report.windows(3).all(|window| {
-                matches!(window[0].abs_diff(window[1]), 1..=3)
-                    && matches!(window[1].abs_diff(window[2]), 1..=3)
-                    && ((window[0] > window[1] && window[1] > window[2])
-                        || (window[0] < window[1] && window[1] < window[2]))
-            })
-        })
-        .count();
-
-    println!("Number of safe reports = {safe_reports}");
+            .collect()
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod utils;
 fn main() {
     match prompt("Select a challenge day: ") {
         1 => select_challenge(day1::solution1, day1::solution2),
-        2 => select_challenge(day2::solution1, || ()),
+        2 => select_challenge(day2::solution1, day2::solution2),
         _ => panic!("Out of bounds day selected!"),
     };
 }
@@ -24,7 +24,11 @@ fn prompt(message: &str) -> u8 {
     print!("{message}");
     let _ = io::stdout().flush();
     let mut s = String::new();
-    io::stdin().read_line(&mut s).expect("Error reading line from stdin!");
+    io::stdin()
+        .read_line(&mut s)
+        .expect("Error reading line from stdin!");
 
-    s.trim().parse::<u8>().expect("Prompt must be a positive integer!")
+    s.trim()
+        .parse::<u8>()
+        .expect("Prompt must be a positive integer!")
 }
