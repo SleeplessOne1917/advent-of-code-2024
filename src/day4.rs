9840ef06ffa2b7623ff1ed8ca88bea3a038c0e14
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-12-05 12:05:56 -0500

    Day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -64,6 +64,33 @@ pub fn solution1() {
     );
 }
 
+pub fn solution2() {
+    let puzzle = read_puzzle();
+
+    let sum = puzzle
+        .windows(3)
+        .map(|window| {
+            zip(
+                window[0].windows(3),
+                zip(window[1].windows(3), window[2].windows(3)),
+            )
+            .map(|(a, (b, c))| (a, b, c))
+            .filter(|tuple| {
+                matches!(
+                    tuple,
+                    ([b'M', _, b'M'], [_, b'A', _], [b'S', _, b'S'])
+                        | ([b'S', _, b'M'], [_, b'A', _], [b'S', _, b'M'])
+                        | ([b'M', _, b'S'], [_, b'A', _], [b'M', _, b'S'])
+                        | ([b'S', _, b'S'], [_, b'A', _], [b'M', _, b'M'])
+                )
+            })
+            .count() as u32
+        })
+        .sum::<u32>();
+
+    println!("X-MAS count = {sum}");
+}
+
 fn read_puzzle() -> Vec<Vec<u8>> {
     read_lines("src/day4/input.txt")
         .map(|line| line.into_bytes())
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         1 => select_challenge(day1::solution1, day1::solution2),
         2 => select_challenge(day2::solution1, day2::solution2),
         3 => select_challenge(day3::solution1, day3::solution2),
-        4 => select_challenge(day4::solution1, || ()),
+        4 => select_challenge(day4::solution1, day4::solution2),
         _ => panic!("Out of bounds day selected!"),
     };
 }
